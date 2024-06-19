4bf0af15228dffc696787c8a520b253c6e5e23aa
40oleg <60510512+40oleg@users.noreply.github.com>
2024-06-19 17:21:50 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,8 +4,7 @@ self.addEventListener('install', event => {
         caches.open('v1').then(cache => {
             return cache.addAll([
                 'index.html',
-                'styles.css',
-                'script.js'
+                'index.js'
             ]);
         })
     );
